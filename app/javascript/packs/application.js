92e124a1eb15e4ee06e5f28291bb59a3e75fc18d
AiriNarita <airiswim.kitty@gmail.com>
2023-02-08 11:29:21 +0900

    feat: swipper-dezign

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "script.js"
+import 'jquery'
+import Swiper from 'swiper/swiper-bundle.js';
+import 'swiper/swiper-bundle.css';
 
 Rails.start()
 Turbolinks.start()
